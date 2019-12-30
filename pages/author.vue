f9c70739051ac9b240476e7a9c5c08b25b16f126
Samuel Austen <furneauxsam1994@gmail.com>
2019-12-30 21:15:58 +0100

    new page

---
+++ pages/author.vue
@@ -0,0 +1,21 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+export default {
+	jsonld() {
+		return {
+			"@context": "http://schema.org",
+			"@type": "Person",
+			name: "Sam Furneaux ",
+			disambiguatingDescription: "41st President of the United States",
+			children: {
+				"@type": "Person",
+				name: "George W. Bush",
+				disambiguatingDescription: "43rd President of the United States"
+			}
+		};
+	}
+};
+</script>
---
+++ pages/travel.vue
@@ -0,0 +1,15 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+export default {
+	jsonld() {
+		return {
+			"@context": "http://schema.org",
+			"@type": "product",
+			name: "My nice product "
+		};
+	}
+};
+</script>
