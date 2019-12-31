15c9e039840ea9e59751d6210944da8360161658
Samuel Austen <furneauxsam1994@gmail.com>
2020-01-01 00:16:48 +0100

    added articles, deleted travel

---
+++ pages/articles/_id.vue
@@ -0,0 +1,98 @@
+<template>
+	<div>
+		<v-parallax max-height="300" :src="$store.state.assetRoot + values.image.path"></v-parallax>
+		<v-container>
+			<v-layout row wrap>
+				<v-flex my-5 xs12 md7>
+					<h1>{{values.title}}</h1>
+					<div v-html="values.content"></div>
+				</v-flex>
+				<v-flex xs12 md5>
+					<Gallery :props="values.gallery"></Gallery>
+				</v-flex>
+			</v-layout>
+		</v-container>
+
+		<!-- Blog -->
+		<v-sheet color="info lighten-1">
+			<v-container>
+				<BlogPosts :props="blogItems"></BlogPosts>
+			</v-container>
+		</v-sheet>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ["props"]
+};
+</script>
+
+<script>
+export default {
+	components: {
+		Gallery: () => import("@/components/Gallery"),
+		BlogPosts: () => import("@/components/BlogPosts")
+	},
+
+	async asyncData({ $axios, route, store }) {
+		let collection = "articles";
+
+		//Get collection
+		let request1 = await $axios.post(
+			store.state.webRoot +
+				"/api/collections/get/" +
+				collection +
+				"?token=" +
+				store.state.collectionsToken,
+			{ filter: { slug: route.params.id } }
+		);
+
+		let request2 = await $axios.post(
+			store.state.webRoot +
+				"/api/collections/get/" +
+				collection +
+				"?token=" +
+				store.state.collectionsToken,
+			{ limit: 5, sort: { _created: -1 } }
+		);
+		return {
+			values: request1.data.entries[0],
+			blogItems: request2.data.entries
+		};
+	},
+	data() {},
+	head() {
+		return {
+			title: this.values.title,
+			meta: [
+				// hid is used as unique identifier. Do not use `vmid` for it as it will not work
+				{
+					hid: this.values.preview,
+					name: this.values.title,
+					content: this.values.preview
+				}
+			]
+		};
+	}
+};
+
+// export default {
+// 	async asyncData({ $axios, route, params}) {
+// 		let collection = "bupaPractises";
+// 		let { data } = await $axios.post(
+// 			$store.state.webRoot +
+// 				"/api/collections/get/" +
+// 				collection +
+// 				"?token=" +
+// 				$store.state.bupaPractisesToken,
+// 			{
+// 				filter: { permalink: route.params.id }
+// 			}
+// 		);
+// 		return { values: data.entries[0] };
+// 	},
+// 	data() {}
+// };
+</script>
+
