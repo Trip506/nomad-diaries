4b92afbdbc12d9ba3d3fa0215798b86691faeb20
Goldenfaeron <tobias@mreich.de>
2020-03-06 18:00:54 +0100

    no message

---
+++ pages/shop.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+{{values}}
+
+    </div>
+
+</template> 
+
+<script>
+export default {
+        async asyncData({ $axios, route, store }) {
+		let collection = "Products";
+
+		//Get collection
+		let request1 = await $axios.post(store.state.webRoot +
+				"/api/collections/get/" +
+				collection +
+				"?token=" +
+				store.state.collectionsToken,
+			);
+
+		return {
+            values: request1.data.entries 
+        };
+    },
+    methods:{
+        fetch(url){
+            var hello=url
+            return hello
+        }
+    },
+
+	data() {
+		return {
+			data: ""
+		};
+	}
+};
+</script>
