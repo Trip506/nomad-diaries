ad7158d2e098ad42e3beaeb506b310d13e67933f
Samuel Austen <furneauxsam1994@gmail.com>
2020-03-06 16:54:57 +0100

    commit

---
+++ pages/swapi.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<div v-for="(item, index) in values" :key="index">{{item}}</div>
+	</div>
+</template>
+
+<script>
+export default {
+	async asyncData({ $axios, route, store }) {
+		let singleton = "privacyPolicy";
+
+		//Get collection
+		let request1 = await $axios.post("https://swapi.co/api/people");
+
+		return {
+			values: request1.data.results
+		};
+	},
+
+	data() {
+		return {
+			data: ""
+		};
+	}
+};
+</script>
