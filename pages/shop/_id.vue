c2f000ca242e1277e9637832baf3b5bcf7ba40fe
Goldenfaeron <tobias@mreich.de>
2020-03-30 17:53:12 +0200

    no message

--- pages/shop/_id.vue
+++ pages/shop/_id.vue
@@ -29,16 +29,14 @@
           <p v-if="values.discount_price==0" class="title" style="margin-right: 15px;">£{{values.price}}</p>
           <p v-else class="title red--text" style="margin-right: 15px; "><span class="grey--text" style="margin-right: 15px; text-decoration-line: line-through;">£{{values.price}}</span>£{{values.discount_price}}</p>
                 {{values.description}}
-
+        
           <v-overflow-btn
-                    :items="dropdown_font"
-                    label="Overflow Btn"
+                    :items="[1,2,3,4,5,6,7,8,9]"
+                    label="Quantity"
                     target="#dropdown-example"
                   ></v-overflow-btn>
           <v-btn flat color="grey lighten-1">Buy Now</v-btn>
 
-   
-  
     </v-flex>
   </v-layout>
     </v-container>
