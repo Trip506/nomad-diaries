cbe362cfdb753d3b827176145da7ffb6e9cb9269
Samuel Austen <furneauxsam1994@gmail.com>
2020-04-10 23:10:35 +0200

    select multiple and delete

--- pages/shop/cms.vue
+++ pages/shop/cms.vue
@@ -10,8 +10,70 @@
 		</v-container>
 		<v-container grid-list-lg>
 			<v-layout row wrap>
-				<v-flex xs12 sm6 lg4 v-for="(item, index) in values" :key="index">
-					<v-card>{{item.name}}</v-card>
+				<v-btn @click="selectMultiple = !selectMultiple">Select Multiple</v-btn>
+
+				<v-btn v-if="selectMultiple" color="red" @click="deleteMultiple()">Delete all</v-btn>
+			</v-layout>
+		</v-container>
+		<v-container grid-list-lg>
+			<v-layout row wrap>
+				<v-flex xs12 sm4 lg3 v-for="(item, index) in values" :key="index">
+					<v-card>
+						<v-checkbox
+							v-if="selectMultiple"
+							label="Select"
+							v-bind="item.selected"
+							v-model="selectedItems"
+							:value="item._id"
+						>Checkbox</v-checkbox>
+						<v-img :src="$store.state.assetRoot + item.image.path" max-height="10cm"></v-img>
+
+						<v-card-title primary-title>
+							<v-layout align-content-space-between justify-space-between>
+								<v-flex>
+									<p class="headline d-flex">{{item.name}}</p>
+								</v-flex>
+
+								<span
+									v-if="item.discount_price!=0"
+									class="title red--text"
+								>{{item.discount_price/item.price*100}}% OFF!</span>
+							</v-layout>
+						</v-card-title>
+
+						<v-card-text>
+							{{item.price}}
+							<span v-show="!item.show">{{item.description | truncate(54)}}</span>
+							<span v-show="item.show">{{item.description}}</span>
+							<span style="margin-left: 45%;">
+								<v-btn icon @click="item.show = !item.show">
+									<v-icon>{{ !item.show ? 'mdi-chevron-down' : 'mdi-chevron-up' }}</v-icon>
+								</v-btn>
+							</span>
+						</v-card-text>
+
+						<!-- v-if="!item.inCart" -->
+						<v-card-actions>
+							<v-btn color="secondary">
+								<nuxt-link :to="'/shop/'+item.slug" class="accent--text">Read more</nuxt-link>
+							</v-btn>
+							<v-btn color="red" @click="deleteItem(item._id)">Delete</v-btn>
+							<v-spacer></v-spacer>
+							<p
+								v-if="item.discount_price==0"
+								class="title"
+								style="margin-right: 15px;"
+							>{{$store.state.currency}}:{{ Math.round($store.state.exchangeRate * item.price )}}</p>
+
+							<p v-else class="title red--text" style="margin-right: 15px; ">
+								<span
+									class="grey--text"
+									style="margin-right: 15px; text-decoration-line: line-through;"
+								>{{$store.state.currency}}:{{Math.round($store.state.exchangeRate * item.price)}}</span>
+								{{$store.state.currency}}:{{Math.round($store.state.exchangeRate * item.discount_price) }}
+							</p>
+						</v-card-actions>
+					</v-card>
 				</v-flex>
 			</v-layout>
 		</v-container>
@@ -37,9 +99,46 @@ export default {
 		};
 	},
 
-	computed: {},
-
 	methods: {
+		async deleteItem(itemId) {
+			let collection = "Products";
+			await this.$axios.post(
+				this.$store.state.webRoot +
+					"/api/collections/remove/" +
+					collection +
+					"?token=" +
+					this.$store.state.masterToken,
+				{
+					filter: { _id: itemId }
+				}
+			);
+			return window.location.reload(true);
+		},
+
+		async deleteMultiple() {
+			// if list has items
+			if (this.selectedItems.length > 0) {
+				let collection = "Products";
+				//send delete request for each item
+				this.selectedItems.forEach(element => {
+					this.$axios.post(
+						this.$store.state.webRoot +
+							"/api/collections/remove/" +
+							collection +
+							"?token=" +
+							this.$store.state.masterToken,
+						{
+							filter: { _id: element }
+						}
+					);
+				});
+
+				//Clear list
+				this.selectedItems = [];
+				return window.location.reload(true);
+			} else return false;
+		},
+
 		async addItem() {
 			let collection = "Products";
 
@@ -250,7 +349,9 @@ export default {
 				"cras"
 			],
 			show: false,
-			data: ""
+			data: "",
+			selectMultiple: false,
+			selectedItems: []
 		};
 	}
 };
